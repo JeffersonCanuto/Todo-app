1d8f98326fefbed2bb32e567674ae55ff2039888
jefferson <jeffersondscanuto@gmail.com>
2025-01-02 15:49:20 -0400

    chore: update todos files

--- backend/django-rest/api/settings.py
+++ backend/django-rest/api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo.apps.TodoConfig'
+    'todos.apps.TodosConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo-app',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '0.0.0.0',
+        'PORT': '5433'
     }
 }
 
