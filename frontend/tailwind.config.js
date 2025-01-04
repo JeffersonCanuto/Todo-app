0d204513c9acf431f842912002dc27b9c4e28255
jefferson <jeffersondscanuto@gmail.com>
2025-01-04 10:25:45 -0400

    feat: add screen sizes to work with responsiveness later on

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,10 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+			center: true,
+			padding: "15px"
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px"
+		},
     extend: {
       fontFamily: {
         exo: ['"Exo 2"', 'sans-serif'],
-      },
+      }
     },
   },
   plugins: [],
