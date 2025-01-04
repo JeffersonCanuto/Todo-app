ef4ad59dbc6ee8e45114ddb3e55b8c0e5879e28e
jefferson <jeffersondscanuto@gmail.com>
2025-01-03 22:24:21 -0400

    feat: create ControlledInput component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import Input from "./components/Input";
+import ControlledInput from "./components/ControlledInput";
 
 const App:React.FC = () => {
   	return (
-    	<div className="h-screen flex items-center justify-center">
-			<form className="w-[80vw] h-[80vh] mx-auto flex justify-center items-center">
-				<Input />
+    	<div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-600 to-blue-300">
+			<form className="w-[50vw] h-[90vh] mx-auto flex flex-col bg-white rounded-3xl">
+				<h3 className="flex justify-center items-center relative top-5">TO-DO REMINDER</h3>
+				<hr className="relative top-10" />
+				<ControlledInput />
 			</form>
 		</div>
   	)
