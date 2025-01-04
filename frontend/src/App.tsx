510d2b5f4d0c943855340ae40c63c94b8056c437
jefferson <jeffersondscanuto@gmail.com>
2025-01-03 22:42:02 -0400

    style: adjust form title style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,8 @@ const App:React.FC = () => {
   	return (
     	<div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-600 to-blue-300">
 			<form className="w-[50vw] h-[90vh] mx-auto flex flex-col bg-white rounded-3xl">
-				<h3 className="flex justify-center items-center relative top-5">TO-DO REMINDER</h3>
-				<hr className="relative top-10" />
+				<h1 className="flex justify-center items-center relative top-7 text-[#76bdd5] text-3xl font-bold">TO-DO REMINDER</h1>
+				<hr className="relative top-12" />
 				<ControlledInput />
 			</form>
 		</div>
