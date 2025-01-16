16604363603669a89e5673c621a0c81667969460
jefferson <jeffersondscanuto@gmail.com>
2025-01-16 19:08:31 -0400

    refactor: update prop typing system in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,49 @@
 import React from 'react';
 
+import { LuNotebookPen } from "react-icons/lu";
+
 import TodoInsert from "./components/TodoInsert";
 
-const TitleProps : { titleType: string, titleHolder: string, titleName: string, titleId: string } = {
+interface TitleProps {
+	titleType: string;
+	titleHolder: string;
+	titleName: string;
+	titleId: string;
+};
+
+const titleProps : TitleProps = {
 	titleType: "title",
 	titleHolder: "Add a title...",
 	titleName: "title",
 	titleId: "title"
-};
+} as const;
 
-const DescriptionProps: { descType: string, descHolder: string, descName: string, descId: string } = {
+interface DescriptionProps {
+	descType: string;
+	descHolder: string;
+	descName: string;
+	descId: string;
+}
+
+const descriptionProps: DescriptionProps = {
 	descType: "description",
 	descHolder: "Add a description...",
 	descName: "description",
 	descId: "description"
-};
+} as const;
 
 const App:React.FC = () => {
   	return (
     	<div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-600 to-blue-300">
 			<form className="w-[48vw] h-[90vh] mx-auto flex flex-col bg-white rounded-3xl">
-				<h1 className="flex justify-center items-center relative top-7 text-[#76bdd5] text-3xl font-bold font-exo">TO-DO NOTES</h1>
+				<h1 className="flex justify-center items-center relative top-7 text-[#76bdd5] text-3xl font-bold font-exo">
+					TO-DO NOTES
+					<LuNotebookPen className="ml-4" />
+				</h1>
 				<hr className="relative top-12" />
 				<TodoInsert
-					{...TitleProps}
-					{...DescriptionProps}
+					{...titleProps}
+					{...descriptionProps}
 				/>
 			</form>
 		</div>
