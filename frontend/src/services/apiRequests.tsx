d257fe1eab4d9dd08689543a599ad66418965ec4
jefferson <jeffersondscanuto@gmail.com>
2025-01-17 21:35:38 -0400

    chore: implement get/read api request method apiRequests file

---
+++ frontend/src/services/apiRequests.tsx
@@ -0,0 +1,34 @@
+/* READ method: list all data */
+type Todo = {
+    id: number;
+    title: string;
+    description: string;
+    completed: boolean;
+}
+
+const readData = async():Promise<Todo[] | undefined> => {
+    try {
+        const
+            host:string = import.meta.env.VITE_API_HOST,
+            port:string = import.meta.env.VITE_API_PORT;
+
+        const response = await fetch(`http://${host}:${port}/api/todo-read/all/`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("Error while fetching data from API...");
+        }
+        
+        return await response.json();
+    } catch(error:any) {
+        console.log(`${error.message}`);
+    }
+}
+
+export {
+    readData
+};
