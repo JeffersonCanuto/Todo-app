d1e7a5a6f4636aa2a455416501c23ae41195a36e
jefferson <jeffersondscanuto@gmail.com>
2025-01-20 20:48:53 -0400

    feat: update to-do models with status and active fields

--- backend/django-rest/todos/models.py
+++ backend/django-rest/todos/models.py
@@ -5,6 +5,8 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     completed = models.BooleanField(default=False, blank=True, null=True)
+    status = models.CharField(max_length=10, default="")
+    active = models.CharField(max_length=10, default="")
 
     def __str__(self):
         return self.title
