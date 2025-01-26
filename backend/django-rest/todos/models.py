d7060fc05b3d46c5042ebfc4ec30b645518280e3
jefferson <jeffersondscanuto@gmail.com>
2025-01-26 16:30:25 -0400

    chore(backend): update to-do models to delete/create column

--- backend/django-rest/todos/models.py
+++ backend/django-rest/todos/models.py
@@ -5,8 +5,8 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     completed = models.BooleanField(default=False, blank=True, null=True)
-    status = models.BooleanField(default=False, blank=True, null=True)
+    pending = models.BooleanField(default=False, blank=True, null=True)
     actions = models.BooleanField(default=False, blank=True, null=True)
-
+    
     def __str__(self):
         return self.title
