809f101ebb83440ac013bc4fe57af44f4abfe0d1
jefferson <jeffersondscanuto@gmail.com>
2025-01-27 18:00:37 -0400

    chore(backend): update default value for pending field in to-do models

--- backend/django-rest/todos/models.py
+++ backend/django-rest/todos/models.py
@@ -5,7 +5,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     completed = models.BooleanField(default=False, blank=True, null=True)
-    pending = models.BooleanField(default=False, blank=True, null=True)
+    pending = models.BooleanField(default=True, blank=True, null=True)
     actions = models.BooleanField(default=False, blank=True, null=True)
     
     def __str__(self):
